967d8ae2aa71bb274a6aab87814f298e5916f8db
Eric Lepki <eric.lepki01@utrgv.edu>
2020-09-24 22:08:16 -0500

    Added Graph of Worldwide cases

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./Map.css";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
 
-function Map() {
-    return (
-        <div className="map">
-            <h1>I am a map</h1>
-        </div>
-    )
+function Map({ center, zoom }) {
+  // Props => center and zoom
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https:{s}.title.openstreetmap.org/{z}/{x}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        ></TileLayer>
+      </LeafletMap>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
